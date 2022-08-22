4df004244702e56408d4e48d5f5d43f69bf501fa
belikon <vbelikon@gmail.com>
2022-08-22 15:08:44 +0300

    Довлен тест на добавление контакта со вспомогательным методом

---
+++ contact.py
@@ -0,0 +1,24 @@
+class Contact:
+    def __init__(self, abon_first_name, abon_middle_name, abon_last_name, abon_nikname, \
+                 company, address, title, \
+                 phone_home, phone_mobile, phone_work, phone_fax, \
+                 email, email2, email3, homepage):
+
+        self.abon_first_name = abon_first_name
+        self.abon_middle_name = abon_middle_name
+        self.abon_last_name = abon_last_name
+        self.abon_nikname = abon_nikname
+
+        self.company = company
+        self.address = address
+        self.title = title
+
+        self.phone_home = phone_home
+        self.phone_mobile = phone_mobile
+        self.phone_work = phone_work
+        self.phone_fax = phone_fax
+
+        self.email = email
+        self.email2 = email2
+        self.email3 = email3
+        self.homepage = homepage
