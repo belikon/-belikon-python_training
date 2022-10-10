aeab25305e4434019d5f4cf5b3cd7f219c42e184
belikon <vbelikon@gmail.com>
2022-10-10 16:25:07 +0300

    Созданы пакеты для разных зон отвественности

--- test/Add_Contact.py
+++ test/Add_Contact.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from contact import Contact
-from application import Application
+from model.contact import Contact
+from fixture.application import Application
 import pytest
 
 @pytest.fixture
